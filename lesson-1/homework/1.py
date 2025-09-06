676bde0723f6b12e8091694035cd55ad926a4a95
Avraam <66767156+AvraamSzaro@users.noreply.github.com>
2025-09-06 15:10:44 +0500

    Add files via upload

---
+++ lesson-1/homework/1.py
@@ -0,0 +1,22 @@
+#Given a side of square. Find its perimeter and area.
+a=4
+perimeter=4*a
+area=a*a
+' '.join(['Perimeter:',str(perimeter),'Area:',str(area)])
+#Given diameter of circle. Find its length.
+d=16
+length=3.14*d
+' '.join(['Length:',str(length)])
+#Given two numbers a and b. Find their mean.
+a=40
+b=100
+mean=(a+b)/2
+' '.join(['Mean:',str(mean)])
+#Given two numbers a and b. Find their sum, product and square of each number.
+a=4
+b=6
+sum_ab=a+b
+product_ab=a*b
+square_a=a*a
+square_b=b*b
+' '.join(['Sum:',str(sum_ab),'Product:',str(product_ab),'Square_A:',str(square_a),'Square_B:',str(square_b)])
